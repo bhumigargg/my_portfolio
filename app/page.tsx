8bd1c5af408d2a4c29613073e42cea752d4e954b
Bhumi Garg <bhumigarg012@gmail.com>
2025-04-11 19:10:39 +0530

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>Bhumi's Portfolio</title>
+      <title>Bhumi&apos; Portfolio</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main>
